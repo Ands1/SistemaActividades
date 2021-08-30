9f4bca22dd3f501668d2c43201adf83243b21582
Ands <1azands@gmail.com>
2021-08-30 11:53:41 -0600

    Add validation field to all models

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,12 @@
 class Activity < ApplicationRecord
     has_many :activity_note, dependent: :delete_all, class_name: "Activity::Note", foreign_key: "activities_id"
     has_many :activity_participant, dependent: :delete_all, class_name: "Activity::Participant", foreign_key: "activities_id"
+
+    validates :name, presence: true, allow_blank: false
+    validates :description, presence: true, allow_blank: false
+    validates :location, presence: true, allow_blank: false
+    validates :total_cost, presence: true, allow_blank: false
+    validates :initial_date, presence: true, allow_blank: false
+    validates :end_date, presence: true, allow_blank: false
+    validates :state, presence: true, allow_blank: false
 end
--- app/models/activity/participant.rb
+++ app/models/activity/participant.rb
@@ -1,4 +1,7 @@
 class Activity::Participant < ApplicationRecord
     belongs_to :user, foreign_key: 'users_id'
     belongs_to :activity, foreign_key: 'activities_id'
+
+    validates :participation_type, presence: true, allow_blank: false
+    validates :contribution, presence: true, allow_blank: false
 end
